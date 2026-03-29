201131068ea52421579ac49844672ed71bee2ab8
xZeGo <xzego@proton.me>
2026-03-29 16:03:54 +0300

    update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,19 +6,18 @@ group = "net.minestom"
 version = "dev"
 
 repositories {
-    mavenLocal()
     mavenCentral()
     maven("https://repo.viaversion.com")
 }
 
 dependencies {
-    implementation("net.minestom:minestom:dev")
+    implementation("net.minestom:minestom:2026.03.25-1.21.11")
     implementation("org.jctools:jctools-core:4.0.5")
     implementation("it.unimi.dsi:fastutil:8.5.18")
 
      implementation("io.netty:netty-all:4.2.12.Final")
-    implementation("com.viaversion:viaversion-common:5.7.2")
-    implementation("com.viaversion:viabackwards-common:5.7.2")
+    implementation("com.viaversion:viaversion-common:5.8.0")
+    implementation("com.viaversion:viabackwards-common:5.8.0")
 
     implementation("com.google.guava:guava:33.2.1-jre")
     implementation("com.google.code.gson:gson:2.11.0")
